916e8fa3f8dc531c0631e9e8e2b8ec270023427b
Mrmharvs <144642278+Mrmharvs@users.noreply.github.com>
2024-10-21 19:43:34 +0800

    Sfaclanding

---
+++ src/pages/components/layout.js
@@ -0,0 +1,14 @@
+import Footer from './Footer'
+import Navbar from './navbar'
+
+
+ 
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar/>
+      <main>{children}</main>
+      <Footer/>
+    </>
+  )
+}
